fe8ff9fda48cd792fc9badd48ef07e514bb4c2c9
DESKTOP-A26OD07\John <john.schmitt9@gmail.com>
2018-01-08 00:00:35 +0100

    Import Projet aMAZEing Init

---
+++ Assets/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        player = GameObject.Find("/Player");
+        //Debug.Log(player.transform.position);
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 vect = new Vector3(player.transform.position.x + offset.x, transform.position.y, transform.position.z);
+        transform.position=vect;
+        //Debug.Log(vect);
+    }
+}
---
+++ Assets/CameraSwitcher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour {
+
+    public Camera firstCamera;
+    public Camera secondCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            firstCamera.enabled = false;
+            secondCamera.enabled = true;
+            Debug.Log("space");
+            Camera temp = secondCamera;
+            secondCamera = firstCamera;
+            firstCamera = temp;
+        }
+
+    }
+}
